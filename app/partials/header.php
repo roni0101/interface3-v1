219636e6e39f9604116c06edd96831e1e06b6844
Foma <Fomenkof7@gmail.com>
2017-05-04 22:02:33 +0200

    header+navbar

--- app/partials/header.php
+++ app/partials/header.php
@@ -1,7 +1,15 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Test</title>
+	<title>notMomondo</title>
+	<link rel="stylesheet" type="text/css" href="./assets/scss/layouts/_navbar.scss">
 </head>
 <body>
+	<div class="navbar">
+		<div class="logo flex-item">
+			<img src="./assets/images/NOT.png">
+			<img src="./assets/images/momondo-logo.png">
+		</div>
 
+		
+	</div>
