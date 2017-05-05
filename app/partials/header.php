29d8dc8f872173d47b34581c5ad2a768a0be1394
Foma <Fomenkof7@gmail.com>
2017-05-05 02:42:52 +0200

    responcive navigation(navbar)

--- app/partials/header.php
+++ app/partials/header.php
@@ -6,11 +6,31 @@
 </head>
 <body>
 	<div class="navbar">
-		<div class="logo flex-item">
+		<div class="navbar-item logo ">
 			<img src="./assets/images/NOT.png">
 			<img src="./assets/images/momondo-logo.png">
 		</div>
+		
+			<div class="navbar-item">
+	          	<a href="#" class="link selected">Flight</a>
+	        </div>
+	        <div class="navbar-item">
+	         	<a href="#" class="link">Account</a>
+	        </div>
+	        <div class="navbar-item">
+	         	<a href="#" class="link">Register</a>
+	         </div>
+	         <div class="navbar-item">
+	          	<a href="#" class="link">Log-In</a>
+	         </div>
 
+
+		    <button>
+	          <!-- Hamburger icon -->
+	          <span class="icon"></span>
+	        </button>
+      </div>
+    </div>
 		
 	</div>
 
